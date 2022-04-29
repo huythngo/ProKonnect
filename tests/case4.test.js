3abff372b4bbb36231ec09e8db05c6e21e023546
Pratyush Kummar <Pratyush.kumar@ttu.edu>
2022-04-28 23:44:26 -0500

    add new test case

--- tests/case4.test.js
+++ tests/case4.test.js
@@ -40,38 +40,22 @@ test("should sign up a new user", async () => {
     }).expect(200)
 })
 
-// @route Post api/auth
-// @desc login to existed account
-test("should login to the existing user", async() => {
-    await request(app).post('/api/auth').send({
-        email: userOne.email,
-        password: userOne.password
-    }).expect(200)
-})
-
-// @route Post api/auth
-// @desc login to non-existed account
-test("should not login to the nonexisting user", async() => {
-    await request(app).post('/api/auth').send({
-        email: "fakeEmail@ttu.edu",
-        password: "1234"
+// @route Post api/users
+// @desc sign up with an existed email
+test("should not sign up with duplicated email", async () => {
+    await request(app).post('/api/users').send({
+        name: userTwo.name,
+        email: userTwo.email,
+        password: userTwo.password
     }).expect(400)
 })
 
-// @route Post api/auth
-// @desc login with wrong format email
-test("should not login with wrong format email", async() => {
-    await request(app).post('/api/auth').send({
-        email: "fake Email",
-        password: "1234"
+// @route Post api/users
+// @desc sign up with wrong format email
+test("should not sign up with wrong format email", async() => {
+    await request(app).post('/api/users').send({
+        name: userTwo.name,
+        email: "fake email",
+        password: userTwo.password
     }).expect(422)
-})
-
-// @route Post api/auth
-// @desc login with wrong password
-test("should not login with wrong password", async() => {
-    await request(app).post('/api/auth').send({
-        email: userOne.email,
-        password: "wrong password"
-    }).expect(400)
 })
